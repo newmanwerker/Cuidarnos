157e339a616f82b3abc41adc1e1b4b86e5869587
Fernando Matus <fernandomatus@Mac-mini-de-Fernando.local>
2025-04-04 02:52:48 -0300

    Creación del welcome y login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'welcome', pathMatch: 'full'},
+  { path: 'login', loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)},
+  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then( m => m.WelcomePageModule)},
 ];
 
 @NgModule({
