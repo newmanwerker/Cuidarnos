b3510499c38c679b417a6e7186f76e42936a72e3
Fernando Matus <fernandomatus@Mac-mini-de-Fernando.local>
2025-05-31 08:55:14 -0400

    fix the routes for backend

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
-
-const authRoutes = require('./routes/auth.routes');
-
 app.use(cors());
 app.use(express.json());
 
+//Rutes
+const authRoutes = require('./routes/auth.routes');
 app.use('/api', authRoutes);
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor backend en http://localhost:${PORT}`);
+//Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => { 
+  console.log(`BE server running on http://localhost:${PORT}`);
 });
