87eff7bb9978a2f0af071ca8d167f1ea81abb70c
Ayastian <se.ayalas@duocuc.cl>
2025-06-14 20:02:34 -0400

    Adds connection pool configuration options
    Introduces maximum connection limit of 10 and an idle timeout of 10 seconds to the database connection pool.
    
    These changes enhance resource management and improve application performance by ensuring unused connections are released promptly.

--- backend/db.js
+++ backend/db.js
@@ -9,7 +9,9 @@ const pool = new Pool({
   port: process.env.PG_PORT,
   ssl: {
     rejectUnauthorized: false
-  }
+  },
+  max: 10, // máximo 10 conexiones
+  idleTimeoutMillis: 10000 // libera conexiones inactivas después de 10s
 });
 
 module.exports = pool;
